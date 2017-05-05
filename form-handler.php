2a3590f8b841b39fb2601fcae03aad0009a320c3
DianaMcO <dianamcode@outlook.com>
2017-05-05 13:34:59 +0100

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,8 @@
+<?php
+
+$name=$_GET['name'];
+
+echo "Hi $name<br>";
+echo "Do you want to modify this page?"
+
+ ?>
---
+++ index.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Simple HTML page</title>
+    </head>
+    <body>
+       
+
+        <form action="form-handler.php"  method="GET">
+            Enter your name <input type="text" name="name" value=""/>
+        </form>
+
+    </body>
+</html>
