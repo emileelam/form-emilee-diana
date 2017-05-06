77aabfac0772b7e8e88153fc4f9c01c499b3cde4
Emilee Lam <emileelam@hotmail.com>
2017-05-06 13:25:53 +0100

    Added to the form

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
         <title>Simple HTML page</title>
     </head>
     <body>
-       
+
 
         <form action="form-handler.php"  method="GET">
             Enter your name <input type="text" name="name" value=""/>
+            Did this work?
+            <input type="checkbox" name="Yes" value="Yes">Yes
+            <input type="checkbox" name="No" value="No">No<br>
+            <input type="submit" value="Submit"/>
         </form>
 
     </body>
